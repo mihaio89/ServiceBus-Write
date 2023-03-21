46b36bcb8495dbf0cab708b7543229624a3ef537
m <->
2023-03-21 09:16:23 -0700

    use command line arguments

--- Program.cs
+++ Program.cs
@@ -10,48 +10,62 @@ namespace ServiceBus
 {
     class Program
     {
+        static string gk_suffix = "";
         static async Task Main(string[] args)
         {
+           // string currentDateTime= DateTime.Now.ToString("yyyyMMddHHmmssfff");
+          //  Console.WriteLine(currentDateTime);
+          //  var stopwatch = Stopwatch.StartNew();
+            
             // Load the configuration file
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
+             //   .AddJsonFile("appsettings.dev.json")
                 .AddJsonFile("appsettings.int.json")
                 .Build();
 
             // Get the configuration values
             var connectionString = configuration["ServiceBus:ServiceBusConnectionString"];
-            Console.WriteLine($"Connection string: {connectionString}");
+            var queueName = configuration["ServiceBus:QueueName"];
 
-            var topicName = configuration["ServiceBus:TopicName"];
-            Console.WriteLine($"Topic name: {topicName}");
 
             // Read the message JSON from file
             var messageJson = await File.ReadAllTextAsync("message.json");
 
-            for (int i = 0; i < 1; i++)
+            
+            int total = int.Parse(args[0]);
+
+            for (int i = 0; i < total; i++)
             {
-                // Sleep for a short time to avoid sending all messages at once
-                await Task.Delay(100);
+                Thread.Sleep(100);
 
                 string currentDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
-                string gk_suffix = "01";
+                Random random = new Random();
+                if (args[1] == "r") {
+                    gk_suffix = random.Next(2) == 0 ? "12" : "34";
+                } else {
+                    gk_suffix = args[1];
+                }
+
 
                 Guid guid = Guid.NewGuid();
                 string guidString = guid.ToString("N") + "-" + gk_suffix;
 
                 string updatedMessageJson = messageJson
-                    .Replace("VALUE-i_ext", currentDateTime)
-                    .Replace("VALUE-GK", guidString);
+                        .Replace("VALUE-i_ext", currentDateTime)
+                        .Replace("VALUE-GK", guidString);
+
+            //   Console.WriteLine(updatedMessageJson);
 
                 // Parse the message JSON into a JObject
                 var messageObject = JObject.Parse(updatedMessageJson);
 
                 // Get the session ID from the "ID" attribute of the message
-                var sessionId = gk_suffix;
+                var sessionId = gk_suffix; //(string)messageObject["it_bit_it"][0]["ID"];
 
                 // Create a ServiceBusClient and a ServiceBusSender
                 await using var client = new ServiceBusClient(connectionString);
-                await using var sender = client.CreateSender(topicName);
+                await using var sender = client.CreateSender(queueName);
 
                 // Create a ServiceBusMessage with the message body and session ID
                 var messageBody = updatedMessageJson;
@@ -63,8 +77,12 @@ namespace ServiceBus
                 // Send the message and print a confirmation message
                 await sender.SendMessageAsync(message);
 
-                Console.WriteLine($"Sent message with SessionId: '{sessionId}' i_ext = '{currentDateTime}', GK = '{guidString}'");
+                Console.WriteLine($"sent message SessionId: '{sessionId}' : i_ext = '{currentDateTime}', GK = '{guidString}'");
+
             }
+
+          //  stopwatch.Stop();
+           // Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
         }
     }
 }
